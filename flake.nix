0b36c763fcae913350f74a164d460d6430cf2593
Liyua <liyua@duck.com>
2025-07-18 21:05:11 +0200

    Migrated to a nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Flake for Pascal Schwietzer's Package for the Arasaka Plymouth Theme";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      sys:
+      let
+        pkgs = nixpkgs.legacyPackages.${sys};
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "arasaka-plymouth";
+          version = "1.0";
+
+          src = nixpkgs.lib.fileset.toSource {
+            root = ./.;
+            fileset = ./.;
+          };
+
+          # dontConfigure = true;
+          # dontBuild = true;
+
+          installPhase = ''
+            		runHook preInstall
+
+            		mkdir -p $out/share/plymouth/themes/arasaka/
+                cp -r $src/* $src/.* $out/share/plymouth/themes/arasaka/
+            		substituteInPlace $out/share/plymouth/themes/arasaka/*.plymouth --replace '@ROOT@' "$out/share/plymouth/themes/arasaka/"
+
+            		runHook postInstall
+            	'';
+        };
+      }
+    );
+}
